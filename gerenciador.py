770058a81f49ddd8a250c0258f2aeb420fb6ceb7
Ayres Roux <devayresrouxj@gmail.com>
2025-05-18 09:00:36 -0300

    aula: adicionando tarefas

--- gerenciador.py
+++ gerenciador.py
@@ -1,3 +1,11 @@
+def adicionar_tarefa(tarefas, nome_tarefa):
+  tarefa = {"tarefa": nome_tarefa, "completada": False}
+  tarefas.append(tarefa)
+  print(f"Tarefa {nome_tarefa} foi adicionada com sucesso!")
+  return
+
+tarefas = []
+
 while True:
   print("\nMenu do Gerenciador de Lista de Tarefas:")
   print("1. Adicionar tarefa")
@@ -9,7 +17,10 @@ while True:
 
   escolha = input("Digite a sua escolha: ")
 
-  if escolha == "6":
+  if escolha == "1":
+    nome_tarefa = input("Digite o nome da tarefa que deseja adicionar: ")
+    adicionar_tarefa(tarefas, nome_tarefa)
+  elif escolha == "6":
     break
 
 print("Programa finalizado")
