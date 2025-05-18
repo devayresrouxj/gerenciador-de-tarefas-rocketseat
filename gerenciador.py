270ea5cfc3ecdf977c3e1640a3f15d16fd429e7e
Ayres Roux <devayresrouxj@gmail.com>
2025-05-18 13:30:55 -0300

    aula: deletar tarefas completadas

--- gerenciador.py
+++ gerenciador.py
@@ -30,6 +30,14 @@ def completar_tarefa(tarefas, indice):
   print(f"Tarefa {indice_tarefa} marcada como completada")
   return
 
+def deletar_tarefas_completadas(tarefas):
+  for tarefa in tarefas:
+    if tarefa["completada"]:
+      tarefas.remove(tarefa)
+      
+  print("Tarefas completadas foram deletadas.")
+  return
+
 tarefas = []
 
 while True:
@@ -60,6 +68,10 @@ while True:
     ver_tarefas(tarefas)
     indice_tarefa = input("Digite o número da tarefa que deseja completar: ")
     completar_tarefa(tarefas, indice_tarefa)
+
+  elif escolha == "5":
+    deletar_tarefas_completadas(tarefas)
+    ver_tarefas(tarefas)
 
   elif escolha == "6":
     break
